79bdcd941173690daf46e1e14809d5761c187055
Purushottam Rathi <purushottam.rathi@perficient.com>
2026-06-17 12:45:56 +0530

    build failed fixed

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+    'next/typescript'
+  ],
+  ignorePatterns: [
+    '.next/',
+    'out/',
+    'build/',
+    'next-env.d.ts'
+  ],
+};
